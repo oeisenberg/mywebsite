28679339768c382eca5fdd71faf3c223e6d95b9e
Oliver <oliver.eisenberg88@gmail.com>
2022-06-13 22:55:32 +0100

    Adding spacing to ext links

--- src/components/PersonalLinks.js
+++ src/components/PersonalLinks.js
@@ -3,17 +3,16 @@ import Button from '@mui/material/Button';
 import Box from "@mui/material/Box";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import {Link} from 'react-router-dom';
 
 const PersonalLinks = () => {
     return (
-        <Box>
-            <Button variant="contained" target="_blank" href="https://www.github.com/oeisenberg" endIcon={<GitHubIcon />}>
+        <Box sx={{mt: 10}} display="flex" justifyContent="flex-end" alignItems="flex-end">
+            <Button sx={{m: 2}} variant="contained" target="_blank" href="https://www.github.com/oeisenberg" endIcon={<GitHubIcon />}>
                 GitHub
             </Button>
 
             
-            <Button variant="contained" target="_blank" href="https://www.linkedin.com/in/olivereisenberg" endIcon={<LinkedInIcon />}>
+            <Button sx={{m: 2}} variant="contained" target="_blank" href="https://www.linkedin.com/in/olivereisenberg" endIcon={<LinkedInIcon />}>
                 LinkedIn
             </Button>
             
