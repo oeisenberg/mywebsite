2ecc54c1673228a516c4688506af53b180077acb
Oliver <oliver.eisenberg88@gmail.com>
2022-06-14 11:25:56 +0100

    Rm unused import

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,7 +12,6 @@ import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import styled from 'styled-components';
 import AvatarIcon from "./../imgs/me.jpg";
 
