b237b233fc4673f272cef918fd6cfb3086df2e8f
Oliver <oliver.eisenberg88@gmail.com>
2022-06-14 18:54:19 +0100

    Adding more details

--- src/pages/MyDissertation.js
+++ src/pages/MyDissertation.js
@@ -1,6 +1,8 @@
-// import "./App.css";
+import "./App.css";
 import Menu from "./components/Menu";
 import PersonalLinks from "./components/PersonalLinks";
+import Button from '@mui/material/Button';
+import ArticleIcon from '@mui/icons-material/Article';
 
 const MyDissertation = () => {
   return (
@@ -9,9 +11,22 @@ const MyDissertation = () => {
         <Menu />
       </header>
       <body className="App-body">
-        MyDissertation
+        <div>
+          <h2>Abstract</h2> {/* Give class defintion for a section - lgrey background w margin? */}
+          Within Formula One, data gathered from Computational Fluid Dynamics (CFD) does not always correspond to the telemetry collected on the racetrack, where a higher correlation between the sources allows for CFD to carry more weight resulting in successful aerodynamic packages. To improve correlation, Formula One teams use wind tunnels with a scale model of their car to reproduce conditionsin both CFD and the racetrack. This paper focuses on the visualisation of data measured from the tunnel using Particle Image Velocimetry. Flow visualisation has undergone lots of research and this document explores the current state of the art and applies vortex extraction and visualisation techniques to a time-dependent data set, provided by Scuderia Toro Rosso, to allow for a meaningful interpretation of data.
+        </div>
+
+        <div> {/* Give class defintion for a section - lgrey background w margin? */}
+          The process led to the revelation of two visualisation techniques using annimation to collapse temporal data into two dimensions. The two visualisation techniques presented are Parallel Annimations and Contextual Plots which can be combined to form a three dimensional Parrallel Contextual Annimation.
+        </div>
+        
+        {/* To Do: Import some images and detials to this page. */}
+      
       </body>
       <footer className="App-footer">
+        <Button sx={{m: 2}} variant="contained" target="_blank" href="https://github.com/oeisenberg/Extraction-and-Visualisation-of-Vortices-from-Instantaneous-Particle-Image-Velocimetry/blob/main/eisenberg-ohe-dissertation-2020-05.pdf" endIcon={<ArticleIcon />}>
+                Dissertation
+        </Button>
         <PersonalLinks />
       </footer>
     </div>
